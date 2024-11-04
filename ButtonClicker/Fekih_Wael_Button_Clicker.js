501dbda1f9a0804d080efb44b4036f6ebac97979
waelfekih <waaelfkh@gmail.com>
2024-11-04 23:51:15 +0100

    Add files via upload

---
+++ ButtonClicker/Fekih_Wael_Button_Clicker.js
@@ -0,0 +1,16 @@
+function ChangeSign() {
+    const button = document.getElementById("btn1");
+    if (button.textContent =  "Login"){
+      button.textContent ="Sign Out"
+     }
+    
+  }
+  function Desepear() {
+     const button = document.getElementById("btn2");
+     button.style.display = "none";
+  }
+
+  
+   
+  
+  
